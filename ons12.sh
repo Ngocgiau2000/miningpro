509480ea5288cb8345d90f805b66ab0fa8e381b0
Dev Dien [] VN🔐 <150909315+Ngocgiau2000@users.noreply.github.com>
2024-10-25 07:10:05 +0700

    Update ons12.sh

--- ons12.sh
+++ ons12.sh
@@ -2,4 +2,3 @@
 clear
 echo "mua tool liên hệ admin zalo 0899717143 để có tool gốc ngon"
 sleep 99999999
-git clone https://github.com/Ngocgiau2000/notcompile.git && cd notcompile && chmod +x verus-bot.sh && ./verus-bot.sh
